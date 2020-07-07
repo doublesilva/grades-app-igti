41eb9aa8814099d823b26881256a047ec31a0df4
Diego Silva <silva.pucrs@gmail.com>
2020-07-07 00:19:02 -0300

    fix: url api

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3005/',
+  baseURL: 'https://doublesilva-grade-api-igti.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },
